85a6d476123ee58b12a7e59df1e84e916d82bc63
antonguzun <anton.guzun@fincollect.cz>
2020-06-17 14:42:48 +0300

    add poetry, fix settings, add linters etc

--- pokemon/urls.py
+++ pokemon/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+
 from pokemon.views import PokemonApiView
 
+
 urlpatterns = [
-    path("pokemon/", PokemonApiView.as_view(), name="pokemon_api"),
+    path("", PokemonApiView.as_view(), name="pokemon_api"),
 ]
--- urls.py
+++ urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("/", ("pokemon.urls", "pokemon", "pokemon")),
-
+    path("pokemon/", ("pokemon.urls", "pokemon", "pokemon")),
 ]
